c9052b60da87334dd0a95d86949cedd5c0a51ab1
Lachlan <talentolachlan@gmail.com>
2022-08-15 16:00:45 -0700

    done with functions

--- app/src/main/java/com/example/pagerapp/MainActivity.java
+++ app/src/main/java/com/example/pagerapp/MainActivity.java
@@ -14,10 +14,14 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -31,11 +35,21 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        // Makes sure it only shows when selected
+        if(savedInstanceState == null) {
+            // Displays Scanner Activity on startup
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ScannerFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_scanner);
+        }
     }
 
     public void onBackPressed() {
@@ -49,5 +63,38 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
 
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+            case R.id.nav_scanner:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ScannerFragment()).commit();
+                break;
+            case R.id.nav_generator:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new GeneratorFragment()).commit();
+                break;
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SettingsFragment()).commit();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this,"Share",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_send:
+                Toast.makeText(this,"Send",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
