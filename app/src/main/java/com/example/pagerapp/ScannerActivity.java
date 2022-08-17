05be8e50b1ce70d7a32efd784b99d59be34025c2
Lachlan <talentolachlan@gmail.com>
2022-08-16 22:40:14 -0700

    finished?

--- app/src/main/java/com/example/pagerapp/ScannerActivity.java
+++ app/src/main/java/com/example/pagerapp/ScannerActivity.java
@@ -1,27 +1,92 @@
 package com.example.pagerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ScannerActivity extends AppCompatActivity {
+
+    private CodeScanner mCodeScanner;
+    String[] permissions = {
+            Manifest.permission.CAMERA
+    };
+    int PERM_CODE = 11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(ScannerActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }
 
-        Bundle bundle = getIntent().getExtras();
-        if(bundle != null) {
-            if(bundle.getString("some") != null) {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
 
-                Toast.makeText(getApplicationContext(), "data:" + bundle.getString("some")
-                        , Toast.LENGTH_SHORT).show();
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+
+    private boolean checkPermissions() {
+
+        List<String> listOfPermissions = new ArrayList<>();
+
+        for(String perm: permissions){
+
+            if(ContextCompat.checkSelfPermission(getApplicationContext(), perm) != PackageManager.PERMISSION_GRANTED) {
+
+                listOfPermissions.add(perm);
 
             }
+        }
+
+        if(!listOfPermissions.isEmpty()){
+
+            ActivityCompat.requestPermissions(this, listOfPermissions.toArray(new
+                    String[listOfPermissions.size()]), PERM_CODE);
+
+            return false;
 
         }
+
+        return true;
 
     }
 }
